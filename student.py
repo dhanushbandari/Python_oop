959924b976dc8f3b0e6279f17046481edfb9d4df
dhanshbandari <dhanushsn17@gmail.com>
2026-03-02 23:04:45 +0530

    Simple update

--- student.py
+++ student.py
@@ -1,8 +1,9 @@
 class Student:
     def __init__(self):
+        # it stores the address of the current executing object
+        # self keyword keeps updating as we create the new objects
         print("I am self..",self) # in this case the adress is :- s1= 0x00000272C566CFE0 and s2=0x00000272C566CFB0
-# it stores the address of the current executing object
-# self keyword keeps updating as we create the new objects
+
 
 s1=Student() 
 print("i am object ref..",s1) # s1= 0x00000272C566CFE0
